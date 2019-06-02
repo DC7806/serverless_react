2bd7a12d40c0700d5999a921ec4d04bf6d327e52
DC <dachang.h@gmail.com>
2019-06-02 15:56:13 +0800

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Blogs from './blogs';
+import Header from './header';
 
 export default class App extends React.Component {
   render(){
     return (
-      <div className="App">
-        <Blogs className="blogs"/>
-      </div>
+      <React.Fragment>
+        <Header/>
+        <div className="container">
+          <Blogs className="blogs"></Blogs>
+        </div>
+      </React.Fragment>
     );  
   }
 }
