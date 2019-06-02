c303a395b9dc69635c73edc0eb3ab7de0dd360f4
DC <dachang.h@gmail.com>
2019-06-02 14:16:05 +0800

    Updates

---
+++ precache-manifest.6929c4ee42064f7c867db301b1e60eaf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1334634d4453077587ca33f0a4d61cac",
+    "url": "/serverless_react/index.html"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/serverless_react/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "eed15c49b80c8edd582e",
+    "url": "/serverless_react/static/js/main.f2b4a435.chunk.js"
+  },
+  {
+    "revision": "8aed13976bb1bf53f069",
+    "url": "/serverless_react/static/js/runtime~main.8c6e176f.js"
+  }
+]);
