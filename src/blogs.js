05eb2c7ef45ec76c573389ae2a8a468eedea31cf
DC <dachang.h@gmail.com>
2019-06-06 12:23:45 +0800

    Add EditForm and FormModal

--- src/blogs.js
+++ src/blogs.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Button from 'react-bootstrap/Button';
+import FormModal from './components/FormModal';
 
 export default class Blogs extends React.Component {
 
   constructor(_props){
     super()
-    this.state = { blogsList: [] }
+    this.state = {
+      blogsList: [],
+      modalShow: false,
+      blogid: '',
+      title: '',
+      content: ''
+    }
     this.fetchData = this.fetchData.bind(this)
   }
 
@@ -25,19 +34,46 @@ export default class Blogs extends React.Component {
     })
   }
 
+  onClick(item) {
+    this.setState({
+      modalShow: true,
+      blogid: item.id,
+      title: item.title,
+      content: item.content
+    })
+  }
+  
   renderList() {
-    return this.state.blogsList.map(({ title, content }, index) => (
-      <Jumbotron key={index} className="blog-item">
-        <h2>{ title }</h2>
-        <p>{ content }</p>
+    return this.state.blogsList.map((item, index) => (
+      <Jumbotron key={item.id} className="blog-item">
+        <h2>{ item.title }</h2>
+        <p>{ item.content }</p>
+        <ButtonToolbar>
+          <Button variant="primary" onClick={() => this.onClick(item)} >
+            Edit
+          </Button>
+        </ButtonToolbar>
       </Jumbotron>
     ))
+  }
+
+  renderModal() {
+    let modalClose = () => this.setState({ modalShow: false })
+
+    return (      
+      <FormModal show={ this.state.modalShow }
+                     onHide={ modalClose }
+                     blogid={ this.state.blogid }
+                     title={ this.state.title }
+                     content={ this.state.content } />
+    )
   }
 
   render() {
     return (
       <div className="blog-list">
         { this.renderList() }
+        { this.renderModal() }
       </div>
     );
   }
