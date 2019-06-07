812bade74be7ad75cc3c722d2c9d681f44f35c3c
DC <dachang.h@gmail.com>
2019-06-07 16:52:02 +0800

    Rename BlogForm to CreateForm

--- src/components/Createform.js
+++ src/components/Createform.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-export default class BlogForm extends React.Component {
+export default class CreateForm extends React.Component {
   constructor(_props) {
     super();
     this.state = {
